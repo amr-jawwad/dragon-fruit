9fb720579b0eb3e756f75438e0bc5a2ed8d4ef95
amr.jawwad <amr.jawwad@outlook.com>
2020-11-08 18:35:47 +0100

    Shifting thinking in difference in days, to difference in hours

--- calculation_functions/CalculateTimeToNextOrder.py
+++ calculation_functions/CalculateTimeToNextOrder.py
@@ -3,23 +3,26 @@ import pandas as pd
 def CalculateTimeToNextOrder(Data: pd.DataFrame, COUNT_FAILED_ORDERS: bool):
     #I had to also sort by customer_rank, because there were orders that were on the same day, at the same hour
     #So the only information about their true order was in the rank.
-    Data = Data.sort_values(['customer_id', 'order_date', 'order_hour','customer_order_rank'])
 
-    #Converting order_date to datetime
-    Data['order_date'] = pd.to_datetime(Data.order_date)
+    #First let's combine order_date and order_hr to create order_time
+
+    Data['order_time'] = pd.to_datetime(Data.order_date + ' ' + Data.order_hour.astype(str) +':00:00')
+
+
+    Data = Data.sort_values(['customer_id', 'order_time','customer_order_rank'])
 
     if COUNT_FAILED_ORDERS:
         #For recalculating the rank
         Data['new_rank'] = 1 #Every order counts!
-        Grouped = Data.groupby('customer_id').agg({"order_date":"diff", "new_rank":"cumsum"})
+        Grouped = Data.groupby('customer_id').agg({"order_time":"diff", "new_rank":"cumsum"})
         Data = Data.drop(['customer_order_rank','new_rank'],axis=1)
         Grouped = Grouped.rename({"new_rank":"customer_order_rank"},axis=1)
     else:
-        Grouped = Data[Data.is_failed==0].groupby('customer_id').agg({"order_date":"diff"})
+        Grouped = Data[Data.is_failed==0].groupby('customer_id').agg({"order_time":"diff"})
 
-    Grouped = Grouped.rename({"order_date":"days_to_next_order"},axis=1)
-    Grouped.days_to_next_order = Grouped.days_to_next_order.shift(-1)
-    Grouped.days_to_next_order = Grouped.days_to_next_order.dt.days
+    Grouped = Grouped.rename({"order_time":"time_to_next_order"},axis=1)
+    Grouped.time_to_next_order = Grouped.time_to_next_order.shift(-1)
+    Grouped.time_to_next_order = (Grouped.time_to_next_order.dt.total_seconds())//3600
 
     Data = pd.merge(Data,Grouped, left_index=True, right_index=True,how='left')
 
