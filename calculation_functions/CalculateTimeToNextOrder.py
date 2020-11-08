9fd35322c2ce6472c5eb34bbfdcaab15e5abccf3
amr.jawwad <amr.jawwad@outlook.com>
2020-11-08 17:21:40 +0100

    Modelling shift from classification to regression problem

--- calculation_functions/CalculateTimeToNextOrder.py
+++ calculation_functions/CalculateTimeToNextOrder.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def CalculateIsReturningCustomer(Data: pd.DataFrame, COUNT_FAILED_ORDERS: bool):
+def CalculateTimeToNextOrder(Data: pd.DataFrame, COUNT_FAILED_ORDERS: bool):
     #I had to also sort by customer_rank, because there were orders that were on the same day, at the same hour
     #So the only information about their true order was in the rank.
     Data = Data.sort_values(['customer_id', 'order_date', 'order_hour','customer_order_rank'])
@@ -17,13 +17,13 @@ def CalculateIsReturningCustomer(Data: pd.DataFrame, COUNT_FAILED_ORDERS: bool):
     else:
         Grouped = Data[Data.is_failed==0].groupby('customer_id').agg({"order_date":"diff"})
 
-    Grouped = Grouped.rename({"order_date":"day_diff"},axis=1)
-    Grouped.day_diff = Grouped.day_diff.shift(-1)
-    Grouped.day_diff = Grouped.day_diff.dt.days
+    Grouped = Grouped.rename({"order_date":"days_to_next_order"},axis=1)
+    Grouped.days_to_next_order = Grouped.days_to_next_order.shift(-1)
+    Grouped.days_to_next_order = Grouped.days_to_next_order.dt.days
 
     Data = pd.merge(Data,Grouped, left_index=True, right_index=True,how='left')
 
-    Data['is_returning_customer'] = 0
-    Data.loc[Data.day_diff<=180,'is_returning_customer'] = 1
+    # Data['is_returning_customer'] = 0
+    # Data.loc[Data.day_diff<=180,'is_returning_customer'] = 1
     
     return Data
--- tests/test_CalculateTimeToNextOrder.py
+++ tests/test_CalculateTimeToNextOrder.py
@@ -2,7 +2,7 @@ import pytest
 import numpy as np
 import pandas as pd
 
-from calculation_functions.CalculateIsReturningCustomer import CalculateIsReturningCustomer
+from calculation_functions.CalculateTimeToNextOrder import CalculateTimeToNextOrder
 
 mock_orders_data = {'customer_id':
                     {0: 'a',
@@ -32,16 +32,16 @@ def is_matching(List1: np.array, List2: np.array):
 
 
 def test_calculate_is_returning_customer_counting_failed():
-    result_df = CalculateIsReturningCustomer(Data= pd.DataFrame(mock_orders_data), COUNT_FAILED_ORDERS= True)
+    result_df = CalculateTimeToNextOrder(Data= pd.DataFrame(mock_orders_data), COUNT_FAILED_ORDERS= True)
 
     assert (is_matching(result_df.customer_order_rank.to_numpy(),[1, 2, 3, 4, 5, 6])), "Some values in the calculated order rank were incorrect"
-    assert (is_matching(result_df.day_diff.to_numpy(),[1.0, 2.0, 1.0, 9.0, 47.0, np.nan])), "Some values in the day_diff were incorrect"
-    assert (is_matching(result_df.is_returning_customer.to_numpy(),[1, 1, 1, 1, 1, 0])), "Some values in the is_returning_customer were incorrect"
+    assert (is_matching(result_df.days_to_next_order.to_numpy(),[1.0, 2.0, 1.0, 9.0, 47.0, np.nan])), "Some values in the day_diff were incorrect"
+    #assert (is_matching(result_df.is_returning_customer.to_numpy(),[1, 1, 1, 1, 1, 0])), "Some values in the is_returning_customer were incorrect"
 
 def test_calculate_is_returning_customer_not_counting_failed():
-    result_df = CalculateIsReturningCustomer(Data= pd.DataFrame(mock_orders_data), COUNT_FAILED_ORDERS= False)
+    result_df = CalculateTimeToNextOrder(Data= pd.DataFrame(mock_orders_data), COUNT_FAILED_ORDERS= False)
 
-    assert (is_matching(result_df.day_diff.to_numpy(),[np.nan, 2.0, 10.0, np.nan, 47.0, np.nan])), "Some values in the day_diff were incorrect"
-    assert (is_matching(result_df.is_returning_customer.to_numpy(),[0, 1, 1, 0, 1, 0])), "Some values in the is_returning_customer were incorrect"
+    assert (is_matching(result_df.days_to_next_order.to_numpy(),[np.nan, 2.0, 10.0, np.nan, 47.0, np.nan])), "Some values in the day_diff were incorrect"
+    #assert (is_matching(result_df.is_returning_customer.to_numpy(),[0, 1, 1, 0, 1, 0])), "Some values in the is_returning_customer were incorrect"
 
 
